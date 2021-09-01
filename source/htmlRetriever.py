2ae0e2bdd0f2f897e71b12831e9ccaf0e8d3bb2a
Jake Lucas <jak11772@esri.com>
2021-09-01 11:13:49 -0400

    adding raise for status guard to the html retriever

--- source/htmlRetriever.py
+++ source/htmlRetriever.py
@@ -4,8 +4,9 @@ class HtmlRetriever:
 
     def __init__(self, link):
         self.link = link
-
-        self.content = requests.get(link).content
+        self.response = requests.get(link)
+        self.response.raise_for_status()
+        self.content = self.response.content
 
     def main(self):
         return self.content
